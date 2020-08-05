c3ed08e2e373c7807cb2b43147beb5f6d0b87a8d
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-08-05 22:20:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
 // Write your code here
+class Breakfast {
+    // _food;
+    // get food() {
+    //     return this._food;
+    // }
+    // set food(value) {
+    //     this._food = value;
+    // }
+    // _drink;
+    // get drink() {
+    //     return this._drink;
+    // }
+    // set drink(value) {
+    //     this._drink = value;
+    // }
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink
+    }
+}
+class Lunch {
+    // _salad;
+    // get salad() {
+    //     return this._salad;
+    // }
+    // set salad(value) {
+    //     this._salad = value;
+    // }
+    // _drink;
+    // get drink() {
+    //     return this._drink;
+    // }
+    // set drink(value) {
+    //     this._drink = value;
+    // }
+    // _soup;
+    // get soup() {
+    //     return this._soup;
+    // }
+    // set soup(value) {
+    //     this._soup = value;
+    // }
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+class Dinner {
+    // _salad;
+    // get salad() {
+    //     return this._salad;
+    // }
+    // set salad(value) {
+    //     this._salad = value;
+    // }
+    // _soup;
+    // get soup() {
+    //     return this._soup;
+    // }
+    // set soup(value) {
+    //     this._soup = value;
+    // }
+    // _entree;
+    // get entree() {
+    //     return this._entree;
+    // }
+    // set entree(value) {
+    //     this._entree = value;
+    // }
+    // _dessert;
+    // get dessert() {
+    //     return this._dessert;
+    // }
+    // set dessert(value) {
+    //     this._dessert = value;
+    // }
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
